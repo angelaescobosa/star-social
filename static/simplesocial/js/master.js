52342c690a5b46d8a2e068ffb3e60c483262b969
angelaescobosa <angela.esc24@gmail.com>
2026-07-31 10:20:42 -0700

    Initial commit - Star Social

---
+++ static/simplesocial/js/master.js
@@ -0,0 +1,22 @@
+VANTA.NET({
+    el: "body",
+
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+
+    minHeight: 200,
+    minWidth: 200,
+
+    scale: 1,
+    scaleMobile: 1,
+
+    color: 0xffffff,
+    backgroundColor: 0x050816,
+
+    points: 14,
+    maxDistance: 20,
+    spacing: 18,
+
+    showDots: true
+});
